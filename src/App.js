25a07f95821a23bfb872f306a3b9cb5a123adc0b
farouq.ayofe <farouq.ayofe@whenthen.com>
2022-04-25 23:05:55 +0100

    Initialize husky

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="react-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React React
+          Learn React
         </a>
       </header>
     </div>
